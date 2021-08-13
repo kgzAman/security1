2fc64b71c7d748ae3b7bbbaff9308a88ceb6dbeb
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-08-13 16:44:45 +0600

    final

--- src/main/java/com/test/security/controller/ToDoController.java
+++ src/main/java/com/test/security/controller/ToDoController.java
@@ -4,6 +4,7 @@ import com.test.security.dto.ToDoDto;
 import com.test.security.model.ToDo;
 import com.test.security.service.ToDoService;
 import lombok.Data;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ public class ToDoController {
     private final ToDoService toDoService;
 
     @GetMapping("/{id}")
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
     public String displayToDo(@PathVariable Long id, Model model){
         ToDo toDo = toDoService.getByID(id);
         model.addAttribute(toDo);
@@ -25,35 +27,43 @@ public class ToDoController {
     }
 
     @GetMapping("/creat")
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
+
     public String creatToDo(){
         return "creatToDo";
     }
 
     @PostMapping("/creat")
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
+
     public String creatToDo(@ModelAttribute (name="todo")ToDoDto toDoDto, Principal principal){
         toDoService.creatNewToDo(toDoDto, principal);
         return "redirect:/";
     }
 
     @PostMapping("/start/{id}")
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
     public String getStarted(@PathVariable Long id){
         toDoService.startToDO(id);
         return "redirect:/toDo/{id}";
     }
 
     @PostMapping("/update/{id}")
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
     public String upDateToDO(@ModelAttribute (name="todo")ToDoDto toDoDto,@PathVariable Long id ){
         toDoService.upDate(toDoDto, id);
         return "redirect:/toDo/{id}";
     }
 
     @PostMapping("/done/{id}")
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
     public String finishJob(@PathVariable Long id){
         toDoService.doneToDo(id);
         return "redirect:/";
     }
 
     @PostMapping("/delete/{id}")
+    @PreAuthorize(value = "hasRole('ROLE_USER')")
     public String deleteTodo(@PathVariable Long id){
         toDoService.delete(id);
         return "redirect:/";
